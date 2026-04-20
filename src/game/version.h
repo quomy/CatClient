74e5a4ed318bb264cc8a8904e409e24ce114284d
Rain Xelelo <rxelelo@outlook.com>
2026-04-20 15:16:54 +0300

    bump to 1.1.0

--- src/game/version.h
+++ src/game/version.h
@@ -20,7 +20,7 @@ extern const char *GIT_SHORTREV_HASH;
 
 // CatClient
 #ifndef CATCLIENT_VERSION
-#define CATCLIENT_VERSION "1.0.7"
+#define CATCLIENT_VERSION "1.1.0"
 #endif
 
 #ifndef TCLIENT_VERSION
